10bf0a80ebc10c37e918a9db996feb3e8ce5c561
Taras Layshchuk <taraslayshchuk@users.noreply.github.com>
2016-01-04 20:08:04 +0200

    1.0.0.dev1

--- es2csv.py
+++ es2csv.py
@@ -22,7 +22,7 @@ FLUSH_BUFFER = 1000  # Chunk of docs to flush in temp file
 CONNECTION_TIMEOUT = 120
 TIMES_TO_TRY = 3
 RETRY_DELAY = 60
-__version__ = '1.0.0.dev0'
+__version__ = '1.0.0.dev1'
 
 
 # Retry decorator for functions with exceptions
