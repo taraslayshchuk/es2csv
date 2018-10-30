a4147bbf11af87703e63d7f24ddb094063a1861d
Bruno Ilponse <bruno.ilponse@gmail.com>
2018-10-30 08:50:18 +0100

    fixed  bug preventing the delimiter to be changed

--- es2csv.py
+++ es2csv.py
@@ -209,7 +209,8 @@ class Es2csv:
             self.num_results = sum(1 for line in codecs.open(self.tmp_file, mode='r', encoding='utf-8'))
             if self.num_results > 0:
                 output_file = codecs.open(self.opts.output_file, mode='a', encoding='utf-8')
-                csv_writer = csv.DictWriter(output_file, fieldnames=self.csv_headers)
+                delimiter = self.opts.delimiter.decode('utf-8') if self.opts.delimiter else ','
+                csv_writer = csv.DictWriter(output_file, fieldnames=self.csv_headers, delimiter=delimiter)
                 csv_writer.writeheader()
                 timer = 0
                 widgets = ['Write to csv ',
