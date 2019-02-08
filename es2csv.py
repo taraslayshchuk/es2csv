b49edbebc8a4de30a710a26acf23aa31ffd48f1e
Eugen Ganshorn <eugen.ganshorn@check24.de>
2019-02-08 15:21:37 +0100

    add http compression

--- es2csv.py
+++ es2csv.py
@@ -56,7 +56,7 @@ class Es2csv:
 
     @retry(elasticsearch.exceptions.ConnectionError, tries=TIMES_TO_TRY)
     def create_connection(self):
-        es = elasticsearch.Elasticsearch(self.opts.url, timeout=CONNECTION_TIMEOUT, http_auth=self.opts.auth,
+        es = elasticsearch.Elasticsearch(self.opts.url, http_compress=True, timeout=CONNECTION_TIMEOUT, http_auth=self.opts.auth,
                                          verify_certs=self.opts.verify_certs, ca_certs=self.opts.ca_certs,
                                          client_cert=self.opts.client_cert, client_key=self.opts.client_key)
         es.cluster.health()
